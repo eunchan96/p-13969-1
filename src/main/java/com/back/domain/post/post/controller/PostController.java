acf5755123ea419fb14672a8257d590d4ce329dd
eunchan96 <taegwon02@gmail.com>
2025-06-23 10:21:13 +0900

    validation step 1

--- src/main/java/com/back/domain/post/post/controller/PostController.java
+++ src/main/java/com/back/domain/post/post/controller/PostController.java
@@ -1,4 +1,4 @@
-package com.back.domain.post.post;
+package com.back.domain.post.post.controller;
 
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -18,7 +19,7 @@ public class PostController {
     @ResponseBody
     public String showWrite() {
         return """
-                <form action="doWrite" target="_blank">
+                <form action="doWrite" target="_blank" method="POST">
                     <input type="text" name="title" placeholder="제목" value="안녕"><br>
                     <textarea name="content" placeholder="내용">반가워.</textarea><br>
                     <button type="submit">작성</button>
@@ -26,10 +27,13 @@ public class PostController {
                 """;
     }
 
-    @GetMapping("/posts/doWrite")
+    @PostMapping("/posts/doWrite")
     @ResponseBody
     @Transactional
     public String doWrite(@RequestParam(defaultValue = "") String title, @RequestParam(defaultValue = "") String content) {
+        if (title.isBlank()) return "제목을 입력해주세요.";
+        if (content.isBlank()) return "내용을 입력해주세요.";
+
         Post post = postService.write(title, content);
 
         return "%d번 글이 작성되었습니다.".formatted(post.getId());
