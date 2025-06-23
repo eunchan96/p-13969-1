08f1431c326725eff3b2e46da2019f722e6846ae
eunchan96 <taegwon02@gmail.com>
2025-06-23 12:29:39 +0900

    validation 10 (Error 순서 정렬)

--- src/main/java/com/back/domain/post/post/controller/PostController.java
+++ src/main/java/com/back/domain/post/post/controller/PostController.java
@@ -11,7 +11,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -24,12 +23,12 @@ public class PostController {
     private final PostService postService;
 
     private String getWriteFormHtml() {
-        return getWriteFormHtml("", "", "", "");
+        return getWriteFormHtml("", "", "");
     }
 
-    private String getWriteFormHtml(String errorFieldName, String errorMessage, String title, String content) {
+    private String getWriteFormHtml(String errorMessage, String title, String content) {
         return """
-                <div style="color:red;">%s</div>
+                <ul style="color:red;">%s</ul>
                 <form action="doWrite" method="POST">
                     <input type="text" name="title" placeholder="제목" value="%s" autofocus><br>
                     <textarea name="content" placeholder="내용">%s</textarea><br>
@@ -37,17 +36,18 @@ public class PostController {
                 </form>
                 
                 <script>
-                    const errorFieldName = "%s";
+                    // 현재까지 나온 모든 폼 검색
+                    const forms = document.querySelectorAll("form");
+                    // 그 중에서 가장 마지막에 나온 폼을 선택
+                    const lastForm = forms[forms.length - 1];
+                    
+                    const errorFieldName = lastForm.previousElementSibling?.querySelector('li')?.dataset?.errorFieldName || '';
                     if(errorFieldName.length > 0) {
-                        // 현재까지 나온 모든 폼 검색
-                        const forms = document.querySelectorAll("form");
-                        // 그 중에서 가장 마지막에 나온 폼을 선택
-                        const lastForm = forms[forms.length - 1];
                         // 해당 폼의 에러 필드에 포커스
                         lastForm[errorFieldName].focus();
                     }
                 </script>
-                """.formatted(errorMessage, title, content, errorFieldName);
+                """.formatted(errorMessage, title, content);
     }
 
     @GetMapping("/posts/write")
@@ -74,15 +74,13 @@ public class PostController {
     @Transactional
     public String doWrite(@Valid WriteForm form, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
-//            FieldError fieldError = bindingResult.getFieldError();
-            String errorFieldName = "title";
             String errorMessage = bindingResult.getFieldErrors()
                     .stream()
-                    .map(FieldError::getDefaultMessage)
+                    .map(fieldError -> (fieldError.getField() + "-" + fieldError.getDefaultMessage()).split("-"))
+                    .map(field -> "<!--%s--><li data-error-field-name=\"%s\">%s</li>".formatted(field[1], field[0], field[2]))
                     .sorted()
-                    .map(message -> message.split("-")[1])
-                    .collect(Collectors.joining("<br>"));
-            return getWriteFormHtml(errorFieldName, errorMessage, form.getTitle(), form.getContent());
+                    .collect(Collectors.joining("\n"));
+            return getWriteFormHtml(errorMessage, form.getTitle(), form.getContent());
         }
         Post post = postService.write(form.getTitle(), form.getContent());
 
