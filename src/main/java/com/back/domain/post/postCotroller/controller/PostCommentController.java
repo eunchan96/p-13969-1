04fb147f9889f4f0a119420f29bc6eb4e8cdebef
eunchan96 <taegwon02@gmail.com>
2025-06-24 16:52:45 +0900

    PUT

--- src/main/java/com/back/domain/post/postCotroller/controller/PostCommentController.java
+++ src/main/java/com/back/domain/post/postCotroller/controller/PostCommentController.java
@@ -13,6 +13,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -69,7 +70,7 @@ public class PostCommentController {
         return "post/postComment/modify";
     }
 
-    @PostMapping("/posts/{postId}/comments/{id}/modify")
+    @PutMapping("/posts/{postId}/comments/{id}/modify")
     @Transactional
     public String modify(
             @PathVariable int postId,
